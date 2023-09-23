71678d8c512893920be50a3c596408ea85588e71
Mehran <mehran.rasulian@gmail.com>
2023-09-23 13:15:19 +0300

    Refactored DeployCommandTest.php

--- tests/Feature/Commands/DeployCommandTest.php
+++ tests/Feature/Commands/DeployCommandTest.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Config;
 
-test('it throw error if forge token env key not defined', function ($keys) {
+test('it throw error if forge required config not prepared', function ($keys) {
     foreach ($keys as $key => $value) {
         Config::set($key, $value);
     }
@@ -14,14 +14,14 @@ test('it throw error if forge token env key not defined', function ($keys) {
         [
             [
                 'services.forge.token' => null,
-                'services.forge.server' => 'asd',
+                'services.forge.server' => 'fake_id',
             ],
             [
-                'services.forge.token' => 'asdasd',
+                'services.forge.token' => 'fake_token',
                 'services.forge.server' => null,
             ],
             [
-                'services.forge.token' => 'asdasd',
+                'services.forge.token' => 'fake_token',
                 'services.forge.server' => 1121,
             ],
         ],
