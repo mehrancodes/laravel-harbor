32cdb31e5bf577a2cbba5849719fcbec00feb944
Mehran <mehran.rasulian@gmail.com>
2023-10-03 19:21:28 +0300

    Added DestroySite.php action to terminate command

---
+++ app/Services/Forge/Pipeline/DestroySite.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Harbor CLI.
+ *
+ * (c) Mehran Rasulian <mehran.rasulian@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace App\Services\Forge\Pipeline;
+
+use App\Services\Forge\ForgeService;
+use App\Traits\Outputifier;
+use Closure;
+
+class DestroySite
+{
+    use Outputifier;
+
+    public function __invoke(ForgeService $service, Closure $next)
+    {
+        $service->site->delete();
+
+        return $next($service);
+    }
+}
