8ce2db59dd1fcbe3336188b07be772409b03c9cb
Shawn Hooper <shawn@fivesense.ca>
2024-03-03 16:33:36 -0500

    DEbug

--- app/Services/Forge/Pipeline/AnnounceSiteOnSlack.php
+++ app/Services/Forge/Pipeline/AnnounceSiteOnSlack.php
@@ -39,7 +39,7 @@ class AnnounceSiteOnSlack
 //
         $this->information('Announce the site on Slack.');
 
-        dd($service->setting->slackChannel, $service->setting->slackBotToken);
+        var_dump($service->setting->slackChannel, $service->setting->slackBotToken);
 
         $route = new SlackRoute($service->setting->slackChannel, $service->setting->slackBotToken);
         Notification::route(SlackChannel::class, $route)
