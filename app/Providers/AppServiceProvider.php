0c33c41278ec7cfbcd112afd930a8613edd7ae83
Mehran <mehran.rasulian@gmail.com>
2024-03-18 17:06:58 +0330

    Updated the translations directory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->app->setLocale('en');
-
-        $this->loadTranslationsFrom(base_path('lang'), 'harbor');
+        //
     }
 
     public function register(): void
--- config/app.php
+++ config/app.php
@@ -57,6 +57,18 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Application Locale Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The application locale determines the default locale that will be used
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
+    |
+    */
+    'locale' => 'en',
+
+    /*
+    |--------------------------------------------------------------------------
     | Autoloaded Service Providers
     |--------------------------------------------------------------------------
     |
