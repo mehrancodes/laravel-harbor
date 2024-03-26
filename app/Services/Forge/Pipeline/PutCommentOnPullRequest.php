5d55ebc01d22d09a46565e8f7e251c886dbdc682
Mehran <mehran.rasulian@gmail.com>
2024-03-26 17:29:18 +0330

    Updated PutCommentOnPullRequest doc

--- app/Services/Forge/Pipeline/PutCommentOnPullRequest.php
+++ app/Services/Forge/Pipeline/PutCommentOnPullRequest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * This file is part of Laravel Harbor.
  *
- * (c) Shvan Sheikha <shvansheikha@gmail.com>
+ * (c) Mehran Rasulian <mehran.rasulian@gmail.com>
  *
  *  For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
