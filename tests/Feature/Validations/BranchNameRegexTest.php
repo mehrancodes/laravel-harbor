1ac64e7fc626950312ac1ff036283c19030912d9
Mehran <mehran.rasulian@gmail.com>
2024-03-26 20:32:45 +0330

    Added validation test for BranchNameRegex

---
+++ tests/Feature/Validations/BranchNameRegexTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Rules\BranchNameRegex;
+use Illuminate\Support\Facades\Validator;
+
+test('it validates the subdomain pattern matches the branch name', function ($data, $failed) {
+    $validator = Validator::make($data, [
+        'branch' => ['required', new BranchNameRegex],
+        'subdomain_pattern' => ['string']
+    ]);
+
+    expect($validator->fails())->toBe($failed);
+})
+    ->with([
+        [
+            'actual' => [
+                'branch' => 'user-notification',
+                'subdomain_pattern' => '/feature\/(.+)/i',
+            ],
+            'failed' => true,
+        ],
+        [
+            'actual' => [
+                'branch' => 'feature/user-notification',
+                'subdomain_pattern' => '/feature\/(.+)/i',
+            ],
+            'failed' => false,
+        ],
+        [
+            'actual' => [
+                'branch' => 'feature/user-notification',
+                'subdomain_pattern' => '',
+            ],
+            'failed' => false,
+        ],
+    ]);
